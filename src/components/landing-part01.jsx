bab5453846c1a37af2bdd485589e0c0345c3d83f
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-11-29 16:06:50 -0500

    add landing with navbar

--- src/components/landing-part01.jsx
+++ src/components/landing-part01.jsx
@@ -12,7 +12,7 @@ export const LandingPart01 = () => {
     useEffect(() => {
         const $rocketIcon = document.getElementById('rocket-icon')
         setTimeout(() => {
-            $rocketIcon.style.transform = `translateY(-32%)`
+            $rocketIcon.style.transform = `translateY(-260px)`
         }, 50)
     }, [])
 
@@ -52,22 +52,21 @@ export const LandingPart01 = () => {
 
 
     return <>
-        <div id='background-landing' className='w-full h-screen flex justify-center'>
+        <div id='background-landing' className='w-full h-screen flex justify-center z-0'>
             <img src={logoBackground} className='landing-logo-bg' />
             <img id='rocket-icon' src={rocketIcon} className='landing-rocket-icon' />
-            <div className='landing-layout'>
 
-                <center className='landing-title'>
-                    IMPULSA TU<br />NEGOCIO
-                </center>
-                <span className='landing-slogan'>
-                    a base de la tecnología web
-                </span>
-                <button className='landing-button-meet'>
-                    AGENDA UNA REUNIÓN
-                </button>
-            </div>
-
+        </div>
+        <div className='landing-layout w-full h-screen flex justify-center z-10'>
+            <center className='landing-title'>
+                IMPULSA TU<br />NEGOCIO
+            </center>
+            <span className='landing-slogan'>
+                a base de la tecnología web
+            </span>
+            <button className='landing-button-meet'>
+                AGENDA UNA REUNIÓN
+            </button>
         </div>
 
     </>
